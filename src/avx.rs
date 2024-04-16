ab57c8a9961d325da435c56739906ac0e408a43f
Mit Bailey <mitbailey99@gmail.com>
2024-04-16 01:11:40 -0400

    Added proper error handling including Result return types; improved command parsing.

--- src/avx.rs
+++ src/avx.rs
@@ -1,6 +1,18 @@
-use serialport::{available_ports, SerialPort, SerialPortType};
-use std::str;
+/**
+ * @file avx.rs
+ * @author Mit Bailey (mitbailey@outlook.com)
+ * @brief
+ * @version See Git tags for version information.
+ * @date 2024.04.15
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+ use serialport::{available_ports, SerialPort, SerialPortType};
+use std::error::Error;
 use std::time::Duration;
+use std::{io, str};
 
 mod coordinates;
 pub use coordinates::{AzmAlt, RADec};
@@ -32,21 +44,40 @@ pub enum SlewDir {
     Negative = 1,
 }
 
+pub enum SlewRate {
+    Stop = 0,
+    Rate1 = 1,
+    Rate2 = 2,
+    Rate3 = 3,
+    Rate4 = 4,
+    Rate5 = 5,
+    Rate6 = 6,
+    Rate7 = 7,
+    Rate8 = 8,
+    Rate9 = 9,
+}
+
 pub struct AdvancedVX {
     port: Box<dyn SerialPort>,
 }
 
 impl Default for AdvancedVX {
     fn default() -> Self {
-        Self::new()
+        Self::new().expect("Failed to create AdvancedVX object.")
     }
 }
 
 impl AdvancedVX {
-    pub fn new() -> AdvancedVX {
+    pub fn new() -> Result<AdvancedVX, io::Error> {
         println!("Available ports:");
 
-        let ports_info = serialport::available_ports().expect("No ports found!");
+        let ports_info = match serialport::available_ports() {
+            Ok(ports) => ports,
+            Err(e) => {
+                eprintln!("Error listing serial ports: {:?}", e);
+                return Err(io::Error::new(io::ErrorKind::Other, e.to_string()));
+            }
+        };
 
         println!("Found {} ports", ports_info.len());
 
@@ -77,63 +108,76 @@ impl AdvancedVX {
             }
         }
 
-        if port_name.is_none() {
-            println!("Device not found.");
-            panic!("Device not found.");
+        match &port_name {
+            Some(p) => println!("Found device: {}", p),
+            None => {
+                return Err(io::Error::new(
+                    io::ErrorKind::NotFound,
+                    "AVX device not found.",
+                ))
+            }
         }
 
-        AdvancedVX {
+        Ok(AdvancedVX {
             // "Software drivers should be prepared to wait up to 3.5s (worst case scenario) for a hand control response."
             port: serialport::new(port_name.unwrap(), 9600)
                 .timeout(Duration::from_millis(3500))
                 .stop_bits(serialport::StopBits::One)
                 .parity(serialport::Parity::None)
                 .open()
-                .expect("Failed to open port"),
-        }
+                .expect("Failed to open port."),
+        })
     }
 
     // Always use the precise variants!
-    pub fn get_position_ra_dec(&mut self) -> Option<RADec> {
-        self.port.write_all(b"e").expect("Failed to write to port");
+    pub fn get_position_ra_dec(&mut self) -> Result<RADec, io::Error> {
+        match self.port.write_all(b"e") {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("Failed to write to port: {:?}", e);
+                return Err(e);
+            }
+        }
 
         let mut serial_buf: Vec<u8> = vec![0; 32];
-        match self
-            .port
-            .read(serial_buf.as_mut_slice())
-            .expect("Found no data!")
-        {
-            0 => {
-                println!("No data found.");
-
-                None // Returns None Option.
+        match self.port.read(serial_buf.as_mut_slice()) {
+            Err(e) => {
+                eprintln!("Failed to read from port: {:?}", e);
+                Err(e)
             }
-            _ => {
+            Ok(0) => {
+                eprintln!("No data found.");
+                Err(io::Error::new(io::ErrorKind::TimedOut, "No data found."))
+            }
+            Ok(_n) => {
                 println!("Data found: {:?}", serial_buf);
-
-                Some(RADec::from_msg(&serial_buf))
+                Ok(RADec::from_msg(&serial_buf))
             }
         }
     }
 
-    fn get_position_azm_alt(&mut self) -> Option<AzmAlt> {
-        self.port.write_all(b"z").expect("Failed to write to port");
+    pub fn get_position_azm_alt(&mut self) -> Result<AzmAlt, io::Error> {
+        match self.port.write_all(b"z") {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("Failed to write to port: {:?}", e);
+                return Err(e);
+            }
+        }
 
         let mut serial_buf: Vec<u8> = vec![0; 32];
-        match self
-            .port
-            .read(serial_buf.as_mut_slice())
-            .expect("Found no data!")
-        {
-            0 => {
-                println!("No data found.");
-
-                None // Returns None Option.
+        match self.port.read(serial_buf.as_mut_slice()) {
+            Err(e) => {
+                eprintln!("Failed to read from port: {:?}", e);
+                Err(e)
             }
-            _ => {
+            Ok(0) => {
+                eprintln!("No data found.");
+                Err(io::Error::new(io::ErrorKind::TimedOut, "No data found."))
+            }
+            Ok(_n) => {
                 println!("Data found: {:?}", serial_buf);
-
-                Some(AzmAlt::from_msg(&serial_buf))
+                Ok(AzmAlt::from_msg(&serial_buf))
             }
         }
     }
@@ -142,7 +186,7 @@ impl AdvancedVX {
     // - AzmAlt will be relative to where it was powered on if not aligned.
     // - Ra/Dec will not work at all if not aligned.
     // degrees as f64 ==> position as i64 ==> Hex String
-    pub fn goto_ra_dec(&mut self, mut coord: RADec) {
+    pub fn goto_ra_dec(&mut self, mut coord: RADec) -> Result<(), io::Error> {
         println!("GOTO: RA: {:?} Dec: {:?}", coord.ra, coord.dec);
         println!(
             "GOTO: RA: {:?} Dec: {:?}",
@@ -155,99 +199,159 @@ impl AdvancedVX {
             coord.absolute_dec()
         );
 
-        self.port
+        match self
+            .port
             .write_all(format!("r{:X},{:X}", coord.absolute_ra(), coord.absolute_dec()).as_bytes())
-            .expect("Failed to write to port");
+        {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                eprintln!("Failed to write to port: {:?}", e);
+                Err(e)
+            }
+        }
     }
 
-    fn goto_azm_alt(&mut self, mut coord: AzmAlt) {
-        self.port
+    pub fn goto_azm_alt(&mut self, mut coord: AzmAlt) -> Result<(), io::Error> {
+        match self
+            .port
             .write_all(format!("r{:X},{:X}", coord.absolute_azm(), coord.absolute_alt()).as_bytes())
-            .expect("Failed to write to port");
+        {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                eprintln!("Failed to write to port: {:?}", e);
+                Err(e)
+            }
+        }
     }
 
     // Need further investigation.
-    pub fn sync(&mut self, mut coord: RADec) {
-        self.port
+    pub fn sync(&mut self, mut coord: RADec) -> Result<(), io::Error> {
+        match self
+            .port
             .write_all(format!("s{:X},{:X}", coord.absolute_ra(), coord.absolute_dec()).as_bytes())
-            .expect("Failed to write to port");
+        {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                eprintln!("Failed to write to port: {:?}", e);
+                Err(e)
+            }
+        }
     }
 
     // 0 = Off
     // 1 = Alt/Az
     // 2 = EQ North
     // 3 = EQ South
-    pub fn get_tracking_mode(&mut self) -> Option<TrackingMode> {
-        self.port.write_all(b"t").expect("Failed to write to port");
-
-        let mut serial_buf: Vec<u8> = vec![0; 32];
-        match self
-            .port
-            .read(serial_buf.as_mut_slice())
-            .expect("Found no data!")
-        {
-            0 => {
-                println!("No data found.");
-
-                None // Returns None Option.
+    pub fn get_tracking_mode(&mut self) -> Result<TrackingMode, io::Error> {
+        match self.port.write_all(b"t") {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("Failed to write to port: {:?}", e);
+                return Err(e);
             }
-            _ => {
-                println!("Data found: {:?}", serial_buf);
+        }
 
+        // This is how read is properly error handled.
+        let mut serial_buf: Vec<u8> = vec![0; 32];
+        match self.port.read(serial_buf.as_mut_slice()) {
+            Err(e) => {
+                eprintln!("Failed to read from port: {:?}", e);
+                Err(e)
+            }
+            // Required to handle the case where read didn't read anything.
+            Ok(0) => {
+                eprintln!("No data found.");
+                Err(io::Error::new(io::ErrorKind::TimedOut, "No data found."))
+            }
+            // Also required to read the exact number of bytes that were read in, even if we don't use it nor care.
+            Ok(_n) => {
+                println!("Data found: {:?}", serial_buf);
                 match serial_buf[0] {
-                    0 => Some(TrackingMode::Off),
-                    1 => Some(TrackingMode::AltAz),
-                    2 => Some(TrackingMode::EQNorth),
-                    3 => Some(TrackingMode::EQSouth),
-                    _ => None,
+                    0 => Ok(TrackingMode::Off),
+                    1 => Ok(TrackingMode::AltAz),
+                    2 => Ok(TrackingMode::EQNorth),
+                    3 => Ok(TrackingMode::EQSouth),
+                    _ => Err(io::Error::new(
+                        io::ErrorKind::InvalidData,
+                        "Invalid tracking mode.",
+                    )),
                 }
             }
         }
     }
 
-    pub fn set_tracking_mode(&mut self, mode: TrackingMode) {
-        self.port
-            .write_all(format!("T{}", mode as u8).as_bytes())
-            .expect("Failed to write to port");
+    pub fn set_tracking_mode(&mut self, mode: TrackingMode) -> Result<(), io::Error> {
+        match self.port.write_all(format!("T{}", mode as u8).as_bytes()) {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                eprintln!("Failed to write to port: {:?}", e);
+                Err(e)
+            }
+        }
     }
 
-    fn slew_variable(&mut self, axis: SlewAxis, dir: SlewDir, rate: u16) {
-        let mut axis_byte = 0;
-        let mut dir_byte = 0;
+    pub fn slew_variable(
+        &mut self,
+        axis: SlewAxis,
+        dir: SlewDir,
+        rate: u16,
+    ) -> Result<(), io::Error> {
+        let axis_byte = match axis {
+            SlewAxis::RAAzm => 16,
+            SlewAxis::DecAlt => 17,
+        };
 
-        match axis {
-            SlewAxis::RAAzm => {
-                axis_byte = 16;
-            }
-            SlewAxis::DecAlt => {
-                axis_byte = 17;
-            }
-        }
-
-        match dir {
-            SlewDir::Positive => {
-                dir_byte = 6;
-            }
-            SlewDir::Negative => {
-                dir_byte = 7;
-            }
-        }
+        let dir_byte = match dir {
+            SlewDir::Positive => 6,
+            SlewDir::Negative => 7,
+        };
 
         let rate_bytes = slew_rate(rate);
 
-        self.port
-            .write_all(
-                format!(
-                    "P{}{}{}{}{}{}{}",
-                    0x3, axis_byte, dir_byte, rate_bytes.0, rate_bytes.1, 0x0, 0x0
-                )
-                .as_bytes(),
+        match self.port.write_all(
+            format!(
+                "P{}{}{}{}{}{}{}",
+                3, axis_byte, dir_byte, rate_bytes.0, rate_bytes.1, 0, 0
             )
-            .expect("Failed to write to port");
+            .as_bytes(),
+        ) {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                eprintln!("Failed to write to port: {:?}", e);
+                Err(e)
+            }
+        }
     }
 
-    fn slew_fixed(&mut self) {
-        
+    pub fn slew_fixed(
+        &mut self,
+        axis: SlewAxis,
+        dir: SlewDir,
+        rate: SlewRate,
+    ) -> Result<(), io::Error> {
+        let axis_byte = match axis {
+            SlewAxis::RAAzm => 16,
+            SlewAxis::DecAlt => 17,
+        };
+
+        let dir_byte = match dir {
+            SlewDir::Positive => 36,
+            SlewDir::Negative => 37,
+        };
+
+        match self.port.write_all(
+            format!(
+                "P{}{}{}{}{}{}{}",
+                2, axis_byte, dir_byte, rate as u8, 0, 0, 0
+            )
+            .as_bytes(),
+        ) {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                eprintln!("Failed to write to port: {:?}", e);
+                Err(e)
+            }
+        }
     }
 
     fn get_location() {}
@@ -283,6 +387,8 @@ impl AdvancedVX {
     fn rtc_set_time() {}
 
     fn get_version() {}
+
+    fn get_device_version() {}
 
     fn get_model() {}
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 mod avx;
 pub use avx::{AdvancedVX, RADec, AzmAlt};
 
-fn main() {
-    let mount = avx::AdvancedVX::new();
-    
+fn main () {
+    println!("Main function called.");
 }
 
 #[cfg(test)]
@@ -12,30 +11,25 @@ mod tests {
     pub use crate::avx::AdvancedVX;
     use crate::avx::RADec;
 
-    // #[test]
-    // fn build() {
-    //     let mount = AdvancedVX::new();
-    // }
-
-    // Example test
     #[test]
     fn get_ra_dec() {
-        let mut mount = AdvancedVX::new();
-        let pos = mount.get_position_ra_dec().unwrap();
+        let mut mount = AdvancedVX::new().expect("Failed to connect to mount.");
+        let pos = mount.get_position_ra_dec().expect("Failed to get position.");
 
         println!("{}, {}",pos.ra, pos.dec);
     }
 
     #[test]
     fn goto_ra_dec() {
-        let mut mount = AdvancedVX::new();
+        let mut mount = AdvancedVX::new().expect("Failed to connect to mount.");
         
-        mount.goto_ra_dec(RADec::new(138.7265968322754, 89.58314180374146));
+        mount.goto_ra_dec(RADec::new(138.7265968322754, 89.58314180374146)).expect("Failed to goto position.");
     }
     
     #[test]
     fn get_tracking_mode() {
-        let mut mount = AdvancedVX::new();
+        let mut mount = AdvancedVX::new().expect("Failed to connect to mount.");
+        
         let mode = mount.get_tracking_mode().unwrap();
 
         println!("{:?}", mode as u8);
